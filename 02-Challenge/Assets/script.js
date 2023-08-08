b70fc9223e59b183615969c617f1205301dd4a5c
amtune <allisontune@gmail.com>
2023-08-08 11:18:20 -0400

    linked classnames and set up link and script tag

---
+++ 02-Challenge/Assets/script.js
@@ -0,0 +1,13 @@
+const search = document.querySelector('.search')
+const searchInput = document.querySelector('.searchInput')
+const history = document.querySelector('.history')
+
+const current = document.querySelector('.current')
+const name = document.querySelector('.name')
+const icon = document.querySelector('.icon')
+const temp = document.querySelector('.temp')
+const hum = document.querySelector('.hum')
+const wind = document.querySelector('.wind')
+
+const forecast = document.querySelector('.forecast')
+const card = document.querySelectorAll('.card')
