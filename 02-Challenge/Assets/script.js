ea3aca170e6a06ada7b63cbd973c24c0d7acb989
amtune <allisontune@gmail.com>
2023-08-08 11:24:58 -0400

    set up javascript layout

--- 02-Challenge/Assets/script.js
+++ 02-Challenge/Assets/script.js
@@ -1,13 +1,43 @@
-const search = document.querySelector('.search')
-const searchInput = document.querySelector('.searchInput')
-const history = document.querySelector('.history')
+const search = document.querySelector('.search');
+const searchInput = document.querySelector('.searchInput');
+const searchBtn = document.querySelector('.searchBtn');
+const history = document.querySelector('.history');
 
-const current = document.querySelector('.current')
-const name = document.querySelector('.name')
-const icon = document.querySelector('.icon')
-const temp = document.querySelector('.temp')
-const hum = document.querySelector('.hum')
-const wind = document.querySelector('.wind')
+const current = document.querySelector('.current');
+const name = document.querySelector('.name');
+const icon = document.querySelector('.icon');
+const temp = document.querySelector('.temp');
+const hum = document.querySelector('.hum');
+const wind = document.querySelector('.wind');
 
-const forecast = document.querySelector('.forecast')
-const card = document.querySelectorAll('.card')
+const forecast = document.querySelector('.forecast');
+const card = document.querySelectorAll('.card');
+
+const key = "59c6b51f552be3550f1167e399494944"
+
+searchBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    let cityName = searchInput.value;
+    fetchCurrent(cityName);
+    fetchForecast(cityName);
+});
+
+function fetchCurrent(city) {
+    fetch()
+    .then(function(res) {
+        return res.json()
+    })
+    .then(function(data) {
+        console.log(data)
+    })
+};
+
+function fetchForecast(city) {
+    fetch()
+    .then(function(res) {
+        return res.json()
+    })
+    .then(function(data) {
+        console.log(data)
+    })
+};
